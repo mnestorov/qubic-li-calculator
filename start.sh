71ffe65f5552b6e3d023d4569991e4a7455a730c
Martin Nestorov <mnestorov@users.noreply.github.com>
2024-04-05 13:40:11 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the script's directory (optional, depending on your setup)
+# cd /path/to/your/script/directory
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run Qubic calculator script
+python main.py
+
+# Deactivate the virtual environment (optional)
+deactivate
